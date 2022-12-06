af14f3b33027154872aa81f94434ed75a58524a9
Sérgio Deivis <gtisergio@gmail.com>
2022-12-06 15:45:38 -0300

    up

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -42,9 +42,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "69e14b7cafd6922e67d23cb5574ba17e",
 "icons/Icon-maskable-192.png": "d19c0a78d3f1542aee8a876424c0d285",
 "icons/Icon-maskable-512.png": "c98bef71dbe1e5fb63041a4c0d496e7d",
-"index.html": "184cd0bafd57506d8bab5dbc511312e7",
-"/": "184cd0bafd57506d8bab5dbc511312e7",
-"main.dart.js": "0f642de9bf43d007ade0d6006997cfe4",
+"index.html": "f5dffe19e85e1263691c680332eb843e",
+"/": "f5dffe19e85e1263691c680332eb843e",
+"main.dart.js": "a4f5aebbef961e7bb173a23fd66e8ea4",
 "manifest.json": "94d429caa0af6c8d8aeb0f74fc2d975c",
 "version.json": "4a6e9be2b3880f990f78153d4043f1f5"
 };
